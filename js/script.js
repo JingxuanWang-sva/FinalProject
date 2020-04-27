73cf074c0822936cf66122648854698ef20dbaf8
JingxuanWang-sva <56276074+JingxuanWang-sva@users.noreply.github.com>
2020-04-27 18:36:49 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-require('aframe');
-require('aframe-animation-component');
-require('aframe-event-set-component')
+const_=require('aframe');
+const_=require('aframe-animation-component');
+const_=require('aframe-event-set-component');
+const_=require('bootstrap');
+const_=require('@gladeye/aframe-preloader-component');
+const_=require('@editvr/aframe-dialog-popup-component');
+
+var AFRAME = require('aframe-core');
+var NoClickLookControls = require('aframe-no-click-look-controls');
+AFRAME.registerComponent('no-click-look-controls', NoClickLookControls);
 
 
 AFRAME.registerComponent('change-color-on-hover', {
