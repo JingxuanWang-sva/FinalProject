670e18278b9404e7292d26f6a7bddf64a9d89b4c
Lauren <laurengibbonz@gmail.com>
2020-05-04 13:46:50 -0400

    lg cleanup

--- js/script.js
+++ js/script.js
@@ -1,23 +1,15 @@
-const_=require('aframe');
-const_=require('aframe-animation-component');
-const_=require('aframe-event-set-component');
-const_=require('bootstrap');
-const_=require('@gladeye/aframe-preloader-component');
-const_=require('@editvr/aframe-dialog-popup-component');
-
 var AFRAME = require('aframe-core');
 var NoClickLookControls = require('aframe-no-click-look-controls');
 AFRAME.registerComponent('no-click-look-controls', NoClickLookControls);
 
-
 AFRAME.registerComponent('change-color-on-hover', {
     schema: {
-      color: {default: 'red'}
+      color: { default: 'red' }
     },
 
     init: function () {
       var data = this.data;
-      var el = this.el;  // <a-box>
+      var el = this.el; 
       var defaultColor = el.getAttribute('material').color;
 
       el.addEventListener('mouseenter', function () {
